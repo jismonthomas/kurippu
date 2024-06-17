41e482aaaec7330e8c89f57c15f4c03733e01769
JISMON THOMAS <jismonthomas18@gmail.com>
2024-06-16 19:36:00 -0700

    account page style update

--- app/dashboard/account/page.js
+++ app/dashboard/account/page.js
@@ -3,7 +3,12 @@ export const metadata = {
 };
 
 const AccountPage = () => {
-  return <div>AccountPage</div>;
+  return (
+    <div className="mx-auto max-w-[800px] p-5">
+      <h1 className="text-xl font-medium">Account Settings</h1>
+      <div className="mt-5 rounded-md border p-5">Coming Soon</div>
+    </div>
+  );
 };
 
 export default AccountPage;
