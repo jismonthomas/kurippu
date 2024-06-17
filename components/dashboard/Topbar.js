a5d630ab554e5c038303f0d0943315c0d99ebd60
JISMON THOMAS <jismonthomas18@gmail.com>
2024-06-16 19:39:54 -0700

    added mobile menu & logo for small screens

--- components/dashboard/Topbar.js
+++ components/dashboard/Topbar.js
@@ -1,15 +1,31 @@
-'use server';
+"use server";
 
-import TopbarProfile from './TopbarProfile';
+import Link from "next/link";
+import TopbarProfile from "./TopbarProfile";
+import { DocumentTextIcon } from "@heroicons/react/24/solid";
+import MobileSidebar from "./sidebar/MobileSidebar";
 
 const Topbar = () => {
-    return (
-        <div className="sticky top-0 left-0 bg-white border-b border-slate-200 px-6">
-            <div className="flex justify-end  min-h-16 items-center">
-                <TopbarProfile />
-            </div>
-        </div>
-    );
+  return (
+    <div className="sticky left-0 top-0 flex items-center border-b border-slate-200 bg-white px-6">
+      {/* mobile menu items */}
+      <div className="flex items-center gap-2 md:hidden">
+        {/* sidebar */}
+        <MobileSidebar />
+        {/* top logo */}
+        <Link href={"/dashboard"} className="flex items-center gap-3 p-2">
+          <div className="inline-block rounded bg-slate-950 p-1">
+            <DocumentTextIcon className="h-3 w-3 text-slate-50" />
+          </div>
+          <h1 className="text-xl font-bold">Kurippu</h1>
+        </Link>
+      </div>
+
+      <div className="ml-auto flex min-h-16 items-center justify-end">
+        <TopbarProfile />
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
