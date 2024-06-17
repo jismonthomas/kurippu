baae1f40e7513fe2b5ba006d26e320296edb4711
JISMON THOMAS <jismonthomas18@gmail.com>
2024-06-16 19:49:46 -0700

    logo updated

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,17 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { DocumentTextIcon } from '@heroicons/react/24/outline';
+import { DocumentTextIcon } from "@heroicons/react/24/solid";
 
 export default function RootLayout({ children }) {
-    return (
-        <>
-            <div className="fixed top-0 left-0 w-full min-h-20 flex items-center px-10">
-                <Link href={`/`} className="flex gap-1">
-                    <DocumentTextIcon className="h-5 w-5 " />
-                    <h1 className=" font-medium">Kurippu</h1>
-                </Link>
-            </div>
-            <div className="min-h-svh flex py-20 ">{children}</div>
-        </>
-    );
+  return (
+    <>
+      <div className="fixed left-0 top-0 flex min-h-20 w-full items-center px-10">
+        <Link href={`/`} className="flex gap-1">
+          <DocumentTextIcon className="h-5 w-5" />
+          <h1 className="font-medium">Kurippu</h1>
+        </Link>
+      </div>
+      <div className="flex min-h-svh py-20">{children}</div>
+    </>
+  );
 }
